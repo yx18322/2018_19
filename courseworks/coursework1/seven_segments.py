8333825e40b3288592dc6e281c8ef025f523a1f2
yx18322 <48391292+yx18322@users.noreply.github.com>
2019-03-11 14:29:12 +0000

    Add files via upload

--- courseworks/coursework1/seven_segments.py
+++ courseworks/coursework1/seven_segments.py
@@ -53,8 +53,8 @@ def seven_segment(pattern):
             number+=pow(2,i)
     print(int(number))
         
-		submission=Submission("Mohit Bhardwaj")
-        submission.header("Mohit Bhardwaj")
+submission=Submission("Mohit_Bhardwaj")
+submission.header("Mohit Bhardwaj")
 
 six=[1,1,-1,1,1,1,1,-1,1,1,-1]
 three=[1,-1,1,1,-1,1,1,1,1,-1,-1]
@@ -64,10 +64,10 @@ seven_segment(three)
 seven_segment(six)
 seven_segment(one)
 
-def createweight(x_v):
-    N = len(x_v)
-    w = np.zeros([N,N])
-    for i in range(N):
+def weight_matrix(x_v):
+N = len(x_v)
+w = np.zeros([N,N])
+for i in range(N):
         for j in range(i,N):
             if i==j:
                 w[i,j] = 0
@@ -76,35 +76,36 @@ def createweight(x_v):
                 w[j,i] = w[i,j]
     return w
 ##this assumes you have called your weight matrix "weight_matrix"
-submission.section("Weight matrix")
-def update(W,weight_matrix):
-    W_ = np.zeros_like(test)
-    N = len(W)
-    m = 0
-    for i in range(N):
-            m = W[i]+np.dot(weight_matrix[i][:],W)
+submission.section("weight matrix")
+def update(W,weight):
+W_ = np.zeros_like(W)
+N = len(W)
+m = 0
+for i in range(N):
+            m = W[i]+np.dot(weight[i][:],W)
             if m > 0:
                 W_[i]=1
             else:
                 W_[i]=-1
  
     return W_
-submission.matrix_print("W",weight_matrix)
-def energy(W,weight_matrix):
-    N = len(W)
-    E = 0
-    for i in range(N):
+
+
+def energy(W,weight):
+N = len(W)
+E = 0
+for i in range(N):
        for j in range(N):
-           E = E-W[i]*weight_matrix[i,j]*W[j]/2.0
+           E = E-W[i]*weight[i,j]*W[j]/2.0
     print(E)
     return E
-
-weight_matrix = (createweight(one) + createweight(three) + createweight(six)) / 3.0
+submission.matrix_print("W",weight)
+weight = (weight_matrix(one) + weight_matrix(three) + weight_matrix(six)) / 3.0
 #print out the energy of the three learned patterns
 print("the energy of the three learned patterns")
-energy(W=one,weight_matrix=weight_matrix)
-energy(W=six,weight_matrix=weight_matrix)
-energy(W=three,weight_matrix=weight_matrix)
+energy(W=one,weight=weight)
+energy(W=six,weight=weight)
+energy(W=three,weight=weight)
 print("test1")
 submission.section("Test 1")
 
@@ -115,20 +116,20 @@ submission.seven_segment(test)
 ##for COMSM0027
 flag =0
 while(flag <1):
-    a=energy(test=test1,weight_matrix=weight_matrix)
-    b=update(test=test1,weight_matrix=weight_matrix)
+    a=energy(test=test1,weight=weight)
+    b=update(test=test1,weight=weight)
    
 ##where energy is the energy of test
 submission.qquad()
 submission.print_number(energy)
- c=energy(test=b,weight_matrix=weight_matrix)
+c=energy(test=b,weight=weight)
    
 ##this prints a space
 submission.qquad()
- if a-c==0:
+if a-c==0:
         print(b)
         flag=0
-    else:
+else:
         flag=2
 
 #here the network should run printing at each step
@@ -145,9 +146,9 @@ seven_segment(test)
 submission.seven_segment(test)
 flag_=0
 while (flag_ < 1):
-    a_=energy(test=test2,weight_matrix=weight_matrix)
-    b_=update(test=test2,weight_matrix=weight_matrix)
-    c_=energy(test=b_,weight_matrix=weight_matrix)
+    a_=energy(test=test2,weight=weight)
+    b_=update(test=test2,weight=weight)
+    c_=energy(test=b_,weight=weight)
     
 ##for COMSM0027
 ##where energy is the energy of test
@@ -156,7 +157,7 @@ submission.print_number(energy)
 if a_-c_==0:
         print(b_)
         flag_=0
-    else:
+else:
         flag_=2
 
 ##this prints a space
